1730bcc18413b2800fa9754ac0e1e58adf847444
Melody J <melodyjacquemin.mj@gmail.com>
2024-07-10 14:04:39 +0200

    email test

--- public/js/reset-pwd.js
+++ public/js/reset-pwd.js
@@ -1,34 +1,34 @@
 // EN COURS...
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   const resetPasswordForm = document.getElementById("reset_password_form");
+document.addEventListener("DOMContentLoaded", function () {
+  const resetPasswordForm = document.getElementById("reset_password_form");
 
-//   resetPasswordForm.addEventListener("submit", function (event) {
-//     event.preventDefault();
+  resetPasswordForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-//     const emailInput = document.getElementById("email");
-//     const email = emailInput.value.trim();
+    const emailInput = document.getElementById("email");
+    const email = emailInput.value.trim();
 
-//     if (!validateEmail(email)) {
-//       displayError("Veuillez entrer une adresse email valide.");
-//       return;
-//     }
+    if (!validateEmail(email)) {
+      displayError("Veuillez entrer une adresse email valide.");
+      return;
+    }
 
-//     // Envoyer le formulaire si la validation passe
-//     this.submit();
-//   });
+    // Envoyer le formulaire si la validation passe
+    this.submit();
+  });
 
-//   function validateEmail(email) {
-//     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-//     return regex.test(email);
-//   }
+  function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
 
-//   function displayError(message) {
-//     const errorElement = document.getElementById("error_message");
-//     if (errorElement) {
-//       errorElement.textContent = message;
-//     } else {
-//       console.error("Element with ID 'error_message' not found");
-//     }
-//   }
-// });
+  function displayError(message) {
+    const errorElement = document.getElementById("error_message");
+    if (errorElement) {
+      errorElement.textContent = message;
+    } else {
+      console.error("Element with ID 'error_message' not found");
+    }
+  }
+});
