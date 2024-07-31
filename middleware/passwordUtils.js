1fbe2f33f5a734d4df240430f0577e6d24882907
Melody J <melodyjacquemin.mj@gmail.com>
2024-07-31 19:29:32 +0200

    css majoritairement

--- middleware/passwordUtils.js
+++ middleware/passwordUtils.js
@@ -1,17 +1,17 @@
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 
-async function hashPassword(password) {
-  const saltRounds = 10;
-  const hashedPassword = await bcrypt.hash(password, saltRounds);
-  return hashedPassword;
-}
+// async function hashPassword(password) {
+//   const saltRounds = 10;
+//   const hashedPassword = await bcrypt.hash(password, saltRounds);
+//   return hashedPassword;
+// }
 
-async function comparePasswords(plainPassword, hashedPassword) {
-  const match = await bcrypt.compare(plainPassword, hashedPassword);
-  return match;
-}
+// async function comparePasswords(plainPassword, hashedPassword) {
+//   const match = await bcrypt.compare(plainPassword, hashedPassword);
+//   return match;
+// }
 
-module.exports = {
-  hashPassword,
-  comparePasswords,
-};
+// module.exports = {
+//   hashPassword,
+//   comparePasswords,
+// };
