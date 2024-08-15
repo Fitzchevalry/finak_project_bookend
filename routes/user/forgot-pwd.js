52f6f6c21ada302b1e7557758b379d495ce8a21a
Melody J <melodyjacquemin.mj@gmail.com>
2024-08-15 13:55:27 +0200

    Reset Password

--- routes/user/forgot-pwd.js
+++ routes/user/forgot-pwd.js
@@ -34,7 +34,7 @@ router.post("/forgot-password", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
-      port: 587,
+      port: 425,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
