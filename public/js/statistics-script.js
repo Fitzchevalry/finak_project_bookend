7f39feb233dc9bcadc9d0ac59f8d4a7fab3125fd
Melody J <melodyjacquemin.mj@gmail.com>
2024-08-22 13:09:58 +0200

    final

--- public/js/statistics-script.js
+++ public/js/statistics-script.js
@@ -29,8 +29,8 @@ document.addEventListener("DOMContentLoaded", () => {
    */
   function initializeStats() {
     try {
-      // const socket = io("https://bookend.koyeb.app");
-      const socket = io();
+      const socket = io("https://bookend.koyeb.app");
+      // const socket = io();
 
       console.log("Socket.IO initialized.");
 
